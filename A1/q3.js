14c07aa4c7be73d4a1b2d98c569089e165dff12a
MapleWorld <ou.ye.canada@hotmail.com>
2017-09-27 16:20:27 -0400

    a3

---
+++ A1/q3.js
@@ -0,0 +1,34 @@
+function unpair(i) {
+	var result = findSum(0, 0, i);
+	var sum = result[0];
+	var next = result[1];
+	var x = i - sum;
+	var y = next - x - 1;
+	console.log("i = " + i + " x = " + x + " y = " + y);
+}
+
+function findSum(sum, next, i) {
+    if (sum + next <= i) {
+    	return findSum(sum + next, next + 1, i)
+    } else {
+    	var result = [sum, next];
+    	return result;
+    }
+}
+
+for (var t = 0; t < 10; t++) {
+	unpair(t);
+}
+//unpair(8);
+/*
+0		0 		0,0
+1		1		0,1
+1		2		1,0
+2		3		0,2
+2		4		1,1
+2		5		2,0
+3		6		0,3
+3		7		1,2
+3		8		2,1
+3		9		3,0
+*/
