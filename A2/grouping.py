a93abbc5bb0fc03f357b4ed536ad88e8cf43a006
MapleWorld <ou.ye.canada@hotmail.com>
2017-10-13 18:28:50 -0400

    q2

---
+++ A2/grouping.py
@@ -0,0 +1,120 @@
+import os
+import sys
+import subprocess
+from subprocess import Popen, PIPE
+
+if len(sys.argv) < 2:
+    sys.exit('Usage: %s <problem size>' % sys.argv[0])
+    
+# Output file
+outputFormulaFileName = "grouping-formula.smt2"
+outputFormulaFile = open(outputFormulaFileName, 'w')
+
+map = dict()
+inputs = []
+
+inputFile = open(sys.argv[1], 'r')
+
+for line in inputFile.readlines():
+    inputs.append(line.replace("\n","").split(" "))
+
+rows = len(inputs);
+cols = len(inputs[0]);
+    
+def buildVarName(row, col):
+    return "m" + str(row) + str(col)
+    
+# Declare variables
+def declareVar():
+    for row in range(0, rows):
+        for col in range(0, cols):
+            if (inputs[row][col] == "*"):
+                continue
+            map[buildVarName(row, col)] = True;
+            outputFormulaFile.write("(declare-const " + buildVarName(row, col) + " Int)\n")
+            if (inputs[row][col] != "-"):
+                outputFormulaFile.write("(assert (= " + buildVarName(row, col) + " " + inputs[row][col] + "))\n")
+                
+            
+
+def formulateZ3Code():
+    declareVar()
+    allGreaterThan0()
+    checkAllNeighbors()
+    noDuplicate()
+    outputFormulaFile.write("(check-sat)\n")
+    outputFormulaFile.write("(get-model)\n")
+    outputFormulaFile.close()
+    
+    
+    
+    
+#executeZ3Code();
+def executeZ3Code(z3Result):
+    # Execute the z3 code and fetch the result
+    list = dict()
+    position = None
+    value = None
+    outputGroupingFileName = "grouping-output.txt"
+    outputGroupingFile = open(outputGroupingFileName, 'w')
+    z3ResultLines = z3Result.split("\n")
+    
+    # Check if there is no solution
+    if "unsat" in z3ResultLines[0]:
+        outputGroupingFile.write("NO SOLUTION")
+        return
+
+    # Parse the output in matrix form
+    for line in z3ResultLines:
+        if "(define-fun" in line:
+            position = line[14:17]
+        if "    " in line:
+            value = line[4:len(line) - 2]
+        if position != None and value != None:
+            list[position] = value
+            position = None
+            value = None
+        
+    for row in range(0, rows):
+        line = ""
+        for col in range(0, cols):
+            str1 = "m" + str(row) + str(col)
+            if list.has_key(str1):
+                line += list[str1] + " "
+            else:
+                line += "* "
+        outputGroupingFile.write(line + "\n")
+
+    outputGroupingFile.close()
+
+formulateZ3Code()
+z3ExecuablePath ='./z3/bin/z3.exe'
+process = Popen([z3ExecuablePath, outputFormulaFileName], stdout=PIPE, stderr=PIPE)
+#z3Result, stderr = process.communicate()
+#executeZ3Code(z3Result)
+#process.terminate()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
