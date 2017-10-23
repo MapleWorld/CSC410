8ab68fb8f007492f20d4acf3aad3181b49e726cf
MapleWorld <ou.ye.canada@hotmail.com>
2017-10-23 14:30:17 -0400

    A3g

---
+++ A3/original/src/UpwardExposedUses.java
@@ -0,0 +1,12 @@
+package csc410.hw3;
+
+/*** Class to hold Upward Exposed Uses analyis. Extend appropriate class. ***/
+
+public class UpwardExposedUses extends ... {
+	
+	// Constructor for class
+	public UpwardExposedUses(DirectedGraph g) {}
+
+	// Override appropriate functions here
+}
+
