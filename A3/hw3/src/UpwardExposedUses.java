23b57b94d36fd00b54950fbd69a592ac28ddbdc8
MapleWorld <ou.ye.canada@hotmail.com>
2017-10-23 00:17:43 -0400

    a3

--- A3/hw3/src/UpwardExposedUses.java
+++ A3/hw3/src/UpwardExposedUses.java
@@ -1,21 +1,16 @@
-import org.jboss.util.Null;
-import org.jboss.util.propertyeditor.IntegerEditor;
 import soot.Local;
 import soot.Unit;
 import soot.ValueBox;
-import soot.toolkits.graph.DirectedGraph;
+import soot.jimple.internal.ImmediateBox;
 import soot.toolkits.graph.UnitGraph;
 import soot.toolkits.scalar.BackwardFlowAnalysis;
 import soot.toolkits.scalar.FlowSet;
 import soot.toolkits.scalar.ArraySparseSet;
-
 import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Iterator;
+import java.util.*;
 
 /*** Class to hold Upward Exposed Uses analyis. Extend appropriate class. ***/
 
@@ -43,116 +38,90 @@ public class UpwardExposedUses extends BackwardFlowAnalysis<Unit, FlowSet<Local>
         File file = new File(fileName);
         BufferedWriter writer;
 
-        ArrayList<String> lst = new ArrayList<>();
-        ArrayList<String> codeLst = new ArrayList<>();
         try {
             writer = new BufferedWriter(new FileWriter(fileName));
-
-            printToConsole(g);
-
-            // Initialize and collect the data needed to write to file
-            HashMap<String, ArrayList<ArrayList<Local>>> blockMap = new HashMap<>();
-
-            for (Unit s : (Iterable<Unit>) g) {
-                //System.out.println(s.getDefBoxes());
-                //System.out.println(s.getUseAndDefBoxes());
-
-                ArrayList<Local> entry = new ArrayList<>();
-                for (Local local : this.getFlowBefore(s)) {
-                    entry.add(local);
-                }
-
-                ArrayList<Local> exit = new ArrayList<>();
-                for (Local local : this.getFlowAfter(s)) {
-                    exit.add(local);
-                }
-
-                ArrayList<ArrayList<Local>> entryExit = new ArrayList<>();
-                entryExit.add(entry);
-                entryExit.add(exit);
-                System.out.println(s.getDefBoxes());
-
-                if (s.getDefBoxes().size() > 0) {
-                    String name = s.getDefBoxes().get(0).toString();
-                    blockMap.put(name, entryExit);
-                    lst.add(name);
-                    codeLst.add(s.toString());
-                }
-            }
-
-            HashMap<String, Integer> trackMap = new HashMap<>();
-
-            // Find the upward exposed uses of the entry value
-            // Loop the code backward
-            for (int i = lst.size() - 1; i >= 0; i--) {
-                String current = lst.get(i);
-
-                // Find the entry set of the block
-                ArrayList<ArrayList<Local>> blockDataList = blockMap.get(current);
-                ArrayList<Local> blockEntryList = blockDataList.get(0);
-
-                // For each entry set, loop each entry set backward
-                for (int j = blockEntryList.size() - 1; j >= 0; j--) {
-                    Local currentEntryVal = blockEntryList.get(j);
-
-                    // Prevent duplicate
-                    if (!trackMap.containsKey(currentEntryVal.toString())) {
-                        trackMap.put(currentEntryVal.toString(), 1);
-                    } else {
-                        continue;
+            for (Unit s : g) {
+                List<ValueBox> useBoxes = s.getUseBoxes();
+                if (useBoxes.size() == 1) {
+                    ValueBox currentUseBox = useBoxes.get(0);
+                    // If it is not integer, then it must be variable assignment
+                    // Then, we search for point where the defintion can reaches
+                    if (valueBoxIsNotInteger(currentUseBox)) {
+                        findAllValidPreSucessors(g, s, currentUseBox, writer);
                     }
-
-                    // Find all line that contains the entry value in their ExitEntrySet
-                    for (int k = 0; k < i; k++) {
-                        ArrayList<ArrayList<Local>>  tempDataList = blockMap.get(lst.get(k));
-                        ArrayList<Local> tempExitList = tempDataList.get(1);
-                        if (tempExitList.contains(currentEntryVal)) {
-                            writer.write(codeLst.get(k) + "\n");
-                            writer.write(codeLst.get(i) + "\n");
-                            writer.write(currentEntryVal.toString() + "\n");
-                            System.out.println("Write");
+                } else if (useBoxes.size() > 1) {
+                    // Find path for each variable used on this line
+                    for (ValueBox currentUseBox : useBoxes) {
+                        if (currentUseBox instanceof ImmediateBox) {
+                            findAllValidPreSucessors(g, s, currentUseBox, writer);
                         }
                     }
-                    System.out.println("Test: " + currentEntryVal.toString() + " " + j + " " + blockEntryList.toString());
                 }
             }
-
-            System.out.println(lst.toString());
-            System.out.println(blockMap.toString());
             writer.close();
-
         } catch (IOException e) {
             e.printStackTrace();
         }
 	}
 
     @SuppressWarnings("unchecked")
-	private void printToConsole(UnitGraph g) {
-        for (Unit s : g) {
-            System.out.print(s);
+	private void findAllValidPreSucessors(UnitGraph g, Unit s, ValueBox useBox, BufferedWriter writer) throws IOException {
+        Queue<Unit> queue = new LinkedList<>();
+        queue.add(s);
+        boolean skippedOnce = false;
+        HashMap<Unit, Boolean> trackMap = new HashMap<>();
 
-            int d = 40 - s.toString().length();
-            while (d > 0) {
-                System.out.print(".");
-                d--;
+        while (queue.size() > 0) {
+            Unit currentS = queue.poll();
+            if (!trackMap.containsKey(currentS)) {
+                if (!skippedOnce) {
+                    skippedOnce = true;
+                } else {
+                    ArrayList<Local> exitSet = findExitSet(currentS);
+                    // Make sure that the variable is still alive
+                    if (exitSet.contains(useBox.getValue())) {
+                        ArrayList<String> strValueBoxList = convertValueBoxToString(currentS.getDefBoxes());
+                        // Make sure to exit when the variable get redefined
+                        if (!strValueBoxList.contains(useBox.getValue().toString())) {
+                            writer.write(currentS.toString() + "\n");
+                            writer.write(s.toString() + "\n");
+                            writer.write(useBox.getValue() + "\n");
+                        } else {
+                            writer.write(currentS.toString() + "\n");
+                            writer.write(s.toString() + "\n");
+                            writer.write(useBox.getValue() + "\n");
+                            continue;
+                        }
+                        if (currentS == s) {
+                            break;
+                        }
+                    }
+                    trackMap.put(currentS, true);
+                }
+                queue.addAll(g.getPredsOf(currentS));
             }
-            FlowSet<Local> set = this.getFlowBefore(s);
-
-            System.out.print("\t[entry: ");
-            for (Local local : set) {
-                System.out.print(local + " ");
-            }
-
-            set = this.getFlowAfter(s);
-
-            System.out.print("]\t[exit: ");
-            for (Local local : set) {
-                System.out.print(local + " ");
-            }
-            System.out.println("]");
         }
     }
 
+    private ArrayList<String> convertValueBoxToString(List<ValueBox> valueBoxes) {
+        ArrayList<String> strList = new ArrayList<>();
+        for (ValueBox current : valueBoxes) {
+            strList.add(current.getValue().toString());
+        }
+        return strList;
+    }
+
+	private boolean valueBoxIsNotInteger(ValueBox valueBox) {
+        String str = valueBox.getValue().toString();
+        try {
+            Integer.parseInt(str);
+        } catch(NumberFormatException e) {
+            return true;
+        } catch(NullPointerException e) {
+            return true;
+        }
+        return false;
+    }
 
 	// Override appropriate functions here
 
@@ -218,5 +187,20 @@ public class UpwardExposedUses extends BackwardFlowAnalysis<Unit, FlowSet<Local>
             }
         }
 	}
+
+    private ArrayList<Local> findExitSet(Unit s ) {
+        ArrayList<Local> exit = new ArrayList<>();
+        for (Local local : this.getFlowAfter(s)) {
+            exit.add(local);
+        }
+        return exit;
+    }
 }
+
+
+
+
+
+
+
 
